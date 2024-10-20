ecf77278381c85dfd46bcda648d0bcd528edd056
Jhon Condor <jhon_elen@outlook.es>
2024-10-20 12:52:16 -0500

    add spring webflux files

---
+++ src/test/java/com/bolsadeideas/springboot/webflux/spring_boot_webflux/SpringBootWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.webflux.spring_boot_webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
