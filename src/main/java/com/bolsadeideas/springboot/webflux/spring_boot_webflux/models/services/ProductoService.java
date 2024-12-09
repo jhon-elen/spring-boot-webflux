9db12deafed77a4df1406c6ba4dc7dd1a7dfb6bb
Jhon Condor <jhon_elen@outlook.es>
2024-12-09 18:57:22 -0500

    add layer Service to persist data

---
+++ src/main/java/com/bolsadeideas/springboot/webflux/spring_boot_webflux/models/services/ProductoService.java
@@ -0,0 +1,20 @@
+package com.bolsadeideas.springboot.webflux.spring_boot_webflux.models.services;
+
+import com.bolsadeideas.springboot.webflux.spring_boot_webflux.models.documents.Producto;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface ProductoService {
+
+    Flux<Producto> findAll();
+
+    Flux<Producto> findAllConNombreUpperCase();
+
+    Flux<Producto> findAllConNombreUpperCaseRepeat();
+
+    Mono<Producto> findById(String id);
+
+    Mono<Producto> save(Producto producto);
+
+    Mono<Void> delete(Producto producto);
+}
