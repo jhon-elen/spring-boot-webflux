c0f80a442876268c64bfc2f006a3328ef6af47a2
Jhon Condor <jhon_elen@outlook.es>
2024-12-12 22:46:00 -0500

    add ver.html view

--- src/main/java/com/bolsadeideas/springboot/webflux/spring_boot_webflux/controllers/ProductoController.java
+++ src/main/java/com/bolsadeideas/springboot/webflux/spring_boot_webflux/controllers/ProductoController.java
@@ -40,6 +40,25 @@ public class ProductoController {
         return service.findAllCategoria();
     }
     
+    @GetMapping("/ver/{id}")
+    public Mono<String> ver(@PathVariable String id, Model model) {
+        
+        return service.findById(id)
+            .doOnNext(p -> {
+                model.addAttribute("producto", p);
+                model.addAttribute("titulo", "Detalle Producto");
+            })
+            .switchIfEmpty(Mono.just(new Producto()))
+            .flatMap(p -> {
+                if (p.getId() == null) {
+                    return Mono.error(new InterruptedException("No existe no encontrado"));
+                }
+                return Mono.just(p);
+            })
+            .then(Mono.just("ver"))
+            .onErrorResume(ex -> Mono.just("redirect:/listar?error=no+existe+el+producto"));
+    }
+    
     @GetMapping({"/listar", "/"})
     public Mono<String> listar(Model model) {
         Flux<Producto> productos = service.findAllConNombreUpperCase();
